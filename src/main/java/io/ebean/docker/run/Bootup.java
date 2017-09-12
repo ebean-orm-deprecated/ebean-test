b0086017f69327cca65a52590a80e7c3bd091379
rob bygrave <robin.bygrave@gmail.com>
2017-09-12 23:13:46 +1200

    Initial

---
+++ src/main/java/io/ebean/docker/run/Bootup.java
@@ -0,0 +1,22 @@
+package io.ebean.docker.run;
+
+import io.ebeaninternal.api.SpiContainerBootup;
+import org.avaje.docker.commands.AutoStart;
+
+/**
+ * Invokes docker commands AutoStart.
+ *
+ * Effectively looks for docker-run.properties and automatically starts appropriate docker containers
+ * (for Postgres, ElasticSearch etc) based on the configuration.
+ */
+public class Bootup implements SpiContainerBootup {
+
+  /**
+   * Start docker containers as configured in docker-run.properties.
+   */
+  public void bootup() {
+
+    AutoStart.run();
+  }
+}
+
---
+++ src/test/java/io/ebean/docker/run/BootupTest.java
@@ -0,0 +1,14 @@
+package io.ebean.docker.run;
+
+import org.junit.Test;
+
+
+public class BootupTest {
+
+  @Test
+  public void bootup() throws Exception {
+
+    new Bootup().bootup();
+  }
+
+}
