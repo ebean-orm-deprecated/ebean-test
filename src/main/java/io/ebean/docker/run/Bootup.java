6fc0b4f65ffdf3a75e1e85a61b6522e5ba341976
Rob Bygrave <robin.bygrave@gmail.com>
2018-01-24 17:29:19 +1300

    #3 - ENH: Add support to read System property ebean_db ... to set appropriate docker container to run against

--- src/main/java/io/ebean/docker/run/Bootup.java
+++ src/main/java/io/ebean/docker/run/Bootup.java
@@ -3,6 +3,9 @@ package io.ebean.docker.run;
 import io.ebeaninternal.api.SpiContainerBootup;
 import org.avaje.docker.container.AutoStart;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Invokes docker commands AutoStart.
  *
@@ -12,11 +15,37 @@ import org.avaje.docker.container.AutoStart;
 public class Bootup implements SpiContainerBootup {
 
   /**
+   * "Docker Run" Known DB containers.
+   */
+  private static List<String> knownContainers = Arrays.asList("postgres", "mysql", "sqlserver");
+
+  /**
    * Start docker containers as configured in docker-run.properties.
    */
   public void bootup() {
 
+    setDockerRunWith();
     AutoStart.run();
+  }
+
+  private void setDockerRunWith() {
+
+    String runWith = System.getProperty("docker_run_with");
+    if (runWith == null) {
+      runWith = System.getProperty("ebean_db");
+      if (isKnownDockerName(runWith)) {
+        // set docker run specific system property such that
+        // it only starts the container we want to run with
+        System.setProperty("docker_run_with", runWith);
+      }
+    }
+  }
+
+  /**
+   * Return true if the ebean_db name is a known docker container name (of docker-run).
+   */
+  private boolean isKnownDockerName(String runWith) {
+    return runWith != null && knownContainers.contains(runWith);
   }
 }
 
--- src/test/java/io/ebean/docker/run/BootupTest.java
+++ src/test/java/io/ebean/docker/run/BootupTest.java
@@ -6,8 +6,15 @@ import org.junit.Test;
 public class BootupTest {
 
   @Test
-  public void bootup() {
+  public void bootup_all() {
     new Bootup().bootup();
   }
 
+  @Test
+  public void bootup_runWith() {
+
+    System.setProperty("ebean_db", "sqlserver");
+
+    new Bootup().bootup();
+  }
 }
