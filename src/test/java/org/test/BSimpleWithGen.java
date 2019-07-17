b56473f93752001f2f0ec6135ee9995030430d29
rob bygrave <robin.bygrave@gmail.com>
2019-07-17 15:50:56 +1200

    #28 - ENH: Add ForTests.rollbackAll(closure) and ForTests.createRollbackAll()

---
+++ src/test/java/org/test/BSimpleWithGen.java
@@ -0,0 +1,48 @@
+package org.test;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import java.util.List;
+import java.util.Map;
+
+@Entity
+public class BSimpleWithGen {
+
+  @Id
+  private Integer id;
+
+  private String name;
+
+  @Transient
+  private Map<String, List<String>> someMap;
+
+  public BSimpleWithGen(String name) {
+    this.name = name;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Map<String, List<String>> getSomeMap() {
+    return someMap;
+  }
+
+  public void setSomeMap(Map<String, List<String>> someMap) {
+    this.someMap = someMap;
+  }
+
+}
